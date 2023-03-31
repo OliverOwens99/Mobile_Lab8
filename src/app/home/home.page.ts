b5f7bce51e7ca67c46a58d7cb5c12430716785a4
Olly <g00409776@atu.ie>
2023-03-31 11:30:58 +0000

    lab8

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { MovieService } from '../movie.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
-  constructor() {}
-  
+  myMovies :any[]=[];
+  constructor(private service: MovieService) { }
+
+  ionViewWillEnter() {
+    this.service.GetMovieData().subscribe(
+      (data)=>{
+        this.myMovies = data.Search;
+      }
+    );
+  }
 }
